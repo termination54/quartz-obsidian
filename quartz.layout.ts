fcd09f704bee61c96aebc4f5e66a924b36b5abd2
termination54 <90281655+termination54@users.noreply.github.com>
2025-07-14 11:12:25 -0400

    Quartz sync: Jul 14, 2025, 11:12 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,8 +24,7 @@ export const sharedPageComponents: SharedLayout = {
   ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Personal Website": "https://nmagliocreative.com/",
     },
   }),
 }
